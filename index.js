2256cae418954911119effb78aea9f50167d3421
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2024-10-27 14:44:56 -0300

    trabalhando com condiçoes e loops

--- index.js
+++ index.js
@@ -6,11 +6,28 @@ app.set('view engine', 'ejs'); //estou dizendo para o Express usar o EJS como vi
 app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Coca-cola", preco:5},
+        {nome: "Leite", preco: 1.45},
+        {nome: "Carne", preco: 15},
+        {nome: "Redbull", preco: 6}
+    ]
+
+
+
+
+
+
     res.render("index", {
         nome: nome,
         lang: lang,
         empresa: "Guia do programador",
-        inscritos: "8000"
+        inscritos: "8000",
+        msg: exibirMsg,
+        produtos:  produtos,
     });
 });
 
