19a762bc44b738222afeb7696d47cac70311bb5c
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2024-10-27 15:31:14 -0300

    Adicionando bootstrap e formulario simples

--- index.js
+++ index.js
@@ -4,33 +4,13 @@ const app = express(); // criando instância do express
 app.set('view engine', 'ejs'); //estou dizendo para o Express usar o EJS como view engine
 app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-cola", preco:5},
-        {nome: "Leite", preco: 1.45},
-        {nome: "Carne", preco: 15},
-        {nome: "Redbull", preco: 6}
-    ]
-
-
-
-
-
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do programador",
-        inscritos: "8000",
-        msg: exibirMsg,
-        produtos:  produtos,
-    });
+app.get("/", (req, res) => { // rota index
+    res.render("index")
 });
+
+app.get("/perguntar", (req, res) => { // rota perguntar
+    res.render("perguntar");
+})
 
 
 
